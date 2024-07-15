6c5d4b63b338644bff14f58a98a90586b0d53edd
Noah Overton <noahoverton123@gmail.com>
2024-07-15 10:38:16 -0500

    Configuring ESLint part 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,17 @@ module.exports = {
   parser: "@typescript-eslint/parser",
 
   extends: [
+    "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
     "plugin:jsx-a11y/recommended",
-    "eslint:recommended",
+    "plugin:react/recommended",
   ],
+
+  plugins: ["react"],
 
   settings: {
     react: {
@@ -29,5 +32,8 @@ module.exports = {
 
   rules: {
     // Custom rules
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "no-unused-vars": ["error", { varsIgnorePattern: "^h$" }],
   },
 };
